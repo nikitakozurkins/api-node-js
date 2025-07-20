79c66ee0f75e08901c8c2f137ebeb398f8f03721
nikita.kozurkins <boba@sosiski.com>
2025-07-20 19:59:40 +0300

    Changed playwright.config.ts to pass CI workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './tests',
+    testDir: './',
     use: {
         baseURL: 'http://localhost:3000/users',  // Adjust to your actual API base URL
     },
